d7fcff9a78f2ed2ee3f137c2afe37a57c04df463
SergioRecioRodriguez <>
2025-08-19 12:07:49 +0000

    updated sandbox notebook

--- sandbox.py
+++ sandbox.py
@@ -1,11 +1,5 @@
 # %%
 import os
-import PIL
-from PIL import Image
-import boto3
-import json
-from io import BytesIO
-import base64
 
 # %%
 # run script to convert files
@@ -13,88 +7,26 @@ import base64
 output_dir = "sample/"
 workers = 4
 format_lines = ""  # "--format_lines"
-use_llm = "--use_llm"
-llm_service = "--llm_service=marker.services.bedrock.BedrockService"
+use_llm = ""  # "--use_llm"
+llm_service = ""  # "--llm_service=marker.services.bedrock.BedrockService"
 llm_model = ""  # "--ollama_model=minicpm-v:8b"
+profile = ""
 
 # %%
-file = "/notebooks/marker/sample-tables.pdf"
+file = "/notebooks/marker/Online_choice_architecture_discussion_paper.pdf"
 
 command = (
-    f"marker_single {file} --output_dir {output_dir} --force_ocr"
+    f"marker_single {file} --output_dir {output_dir} "
+    # "--force_ocr "
     f"{format_lines} "
-    f"{use_llm} {llm_service} {llm_model}"
-    "--bedrock_profile personal"
-    "--debug"
+    f"{use_llm} {llm_service} {llm_model} "
+    f"--bedrock_profile {profile} "
+    "--debug "
+    " --output_format markdown"
 )
 print(command)
 
 # %%
 os.system(command)
-
-# %%
-bedrock_runtime = boto3.client(
-    service_name="bedrock-runtime",
-    region_name="eu-west-2",
-)
-
-
-# %%
-def img_to_base64(img: PIL.Image.Image):
-    image_bytes = BytesIO()
-    img.save(image_bytes, format="WEBP")
-    return base64.b64encode(image_bytes.getvalue()).decode("utf-8")
-
-
-# %%
-base64_string = img_to_base64(Image.open("a.png"))
-
-# %%
-system_prompt = "All your output must be pirate speech"
-prompt_config = {
-    "anthropic_version": "bedrock-2023-05-31",
-    "max_tokens": 4096,
-    "system": system_prompt,
-    "messages": [
-        {
-            "role": "user",
-            "content": [
-                {
-                    "type": "image",
-                    "source": {
-                        "type": "base64",
-                        "media_type": "image/jpeg",
-                        "data": base64_string,
-                    },
-                },
-                {"type": "text", "text": "what is this image?"},
-            ],
-        }
-    ],
-    "temperature": 0.2,
-}
-
-bedrock_session = boto3.session.Session(profile_name="publicprocurement_production")
-
-bedrock_runtime = bedrock_session.client(service_name="bedrock-runtime")
-
-body = json.dumps(prompt_config)
-
-response = bedrock_runtime.invoke_model(
-    body=body,
-    modelId="anthropic.claude-3-sonnet-20240229-v1:0",
-    accept="application/json",
-    contentType="application/json",
-)
-
-response_body = json.loads(response.get("body").read())
-
-results = response_body.get("content")[0].get("text")
-
-# %%
-results
-
-# %%
-response_body.get("usage").get("output_tokens")
 
 # %%
